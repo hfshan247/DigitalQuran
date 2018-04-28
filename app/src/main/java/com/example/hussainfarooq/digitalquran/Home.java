48cbbbc89e5edd460242b0941c67da8f4b3c0a0a
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2018-04-29 00:37:56 +0500

    Implemented Quran reader module

--- app/src/main/java/com/example/hussainfarooq/digitalquran/Home.java
+++ app/src/main/java/com/example/hussainfarooq/digitalquran/Home.java
@@ -1,6 +1,5 @@
 package com.example.hussainfarooq.digitalquran;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -11,12 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.hussainfarooq.digitalquran.model.Quran;
+import com.example.hussainfarooq.digitalquran.model.SurahMeta;
+import com.example.hussainfarooq.digitalquran.util.StableArrayAdapter;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class Home extends AppCompatActivity {
@@ -75,16 +75,14 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        ListView mSurahList = findViewById(R.id.surah_list);
-        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile"};
+        // Load Quran
+        Quran mQuran = Quran.getInstance(this);
 
+        // Display Chapter Names
+        ListView mSurahList = findViewById(R.id.surah_list);
         final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
+        for (SurahMeta meta : mQuran.getMetadata()) {
+            list.add(meta.getTitle());
         }
         final StableArrayAdapter adapter = new StableArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
@@ -94,16 +92,10 @@ public class Home extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        });
+                // Open chapter on click
+                Intent mSurahIntent = new Intent(Home.this, SurahActivity.class);
+                mSurahIntent.putExtra("surahIndex", position + 1);
+                startActivity(mSurahIntent);
             }
         });
 
@@ -154,29 +146,5 @@ public class Home extends AppCompatActivity {
         }
     }
 
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-    }
 
 }
---
+++ app/src/main/java/com/example/hussainfarooq/digitalquran/model/Quran.java
@@ -0,0 +1,66 @@
+package com.example.hussainfarooq.digitalquran.model;
+
+import android.app.Activity;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author saifkhichi96
+ * @version 1.0
+ *          created on 29/04/2018 12:22 AM
+ */
+
+public class Quran {
+    private static Quran ourInstance;
+
+    public static Quran getInstance(Activity activity) {
+        if (ourInstance == null) {
+            ourInstance = new Quran(activity);
+        }
+        return ourInstance;
+    }
+
+    private SurahMeta[] metadata;
+
+    private Quran(Activity activity) {
+        try {
+            metadata = loadMetadata(activity);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private SurahMeta[] loadMetadata(Activity activity) throws IOException {
+        String json = loadJsonString(activity, "quran/surah.json");
+        Gson gson = new Gson();
+        return gson.fromJson(json, SurahMeta[].class);
+    }
+
+    public SurahMeta[] getMetadata() {
+        return metadata;
+    }
+
+    public Surah getSurah(Activity activity, int surahIndex) {
+        try {
+            String json = loadJsonString(activity, "quran/surah/surah_" + String.valueOf(surahIndex) + ".json");
+            Gson gson = new Gson();
+            return gson.fromJson(json, Surah.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String loadJsonString(Activity activity, String filename) throws IOException {
+        InputStream is = activity.getAssets().open(filename);
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        return new String(buffer, "UTF-8");
+    }
+
+}
