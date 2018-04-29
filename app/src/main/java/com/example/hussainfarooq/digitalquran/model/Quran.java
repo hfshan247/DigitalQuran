9d7b7a67ad0f3bd5ac313ed40c472bf92e622ed6
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2018-04-29 10:50:00 +0500

    Added search functions

--- app/src/main/java/com/example/hussainfarooq/digitalquran/model/Quran.java
+++ app/src/main/java/com/example/hussainfarooq/digitalquran/model/Quran.java
@@ -3,11 +3,15 @@ package com.example.hussainfarooq.digitalquran.model;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 
 import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author saifkhichi96
@@ -17,14 +21,7 @@ import java.io.InputStream;
 
 public class Quran {
     private static Quran ourInstance;
-
-    public static Quran getInstance(Activity activity) {
-        if (ourInstance == null) {
-            ourInstance = new Quran(activity);
-        }
-        return ourInstance;
-    }
-
+    private final HashMap<Integer, Surah> surahs = new HashMap<>();
     private SurahMeta[] metadata;
     private Topic[] topics;
 
@@ -32,9 +29,17 @@ public class Quran {
         try {
             metadata = loadMetadata(activity);
             topics = loadTopics(activity);
+            new SurahLoader(activity).execute();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static Quran getInstance(Activity activity) {
+        if (ourInstance == null) {
+            ourInstance = new Quran(activity);
+        }
+        return ourInstance;
     }
 
     private SurahMeta[] loadMetadata(Activity activity) throws IOException {
@@ -79,7 +84,19 @@ public class Quran {
         return topics;
     }
 
-    public Surah getSurah(Activity activity, int surahIndex) {
+    private void loadSurahs(Activity activity) {
+        for (int i = 1; i <= 114; i++) {
+            if (surahs.containsKey(i)) { // Skip Surah which have been already loaded
+                continue;
+            }
+
+            synchronized (surahs) {
+                surahs.put(i, loadSurah(activity, i));
+            }
+        }
+    }
+
+    private synchronized Surah loadSurah(Activity activity, int surahIndex) {
         try {
             String json = loadJsonString(activity, "quran/surah/surah_" + String.valueOf(surahIndex) + ".json");
             Gson gson = new Gson();
@@ -90,6 +107,20 @@ public class Quran {
         return null;
     }
 
+    public Surah getSurah(Activity activity, int surahIndex) {
+        if (surahs.containsKey(surahIndex)) {
+            return surahs.get(surahIndex);
+        } else {
+            Surah surah = loadSurah(activity, surahIndex);
+            synchronized (surahs) {
+                if (surah != null && !surahs.containsKey(surahIndex)) {
+                    surahs.put(surahIndex, surah);
+                }
+            }
+            return surah;
+        }
+    }
+
     private String loadJsonString(Activity activity, String filename) throws IOException {
         InputStream is = activity.getAssets().open(filename);
         int size = is.available();
@@ -97,6 +128,37 @@ public class Quran {
         is.read(buffer);
         is.close();
         return new String(buffer, "UTF-8");
+    }
+
+    public List<Ayat> search(String keyword) {
+        List<Ayat> searchResult = new ArrayList<>();
+        synchronized (surahs) {
+            for (Surah surah : surahs.values()) {
+                searchResult.addAll(surah.search(keyword));
+            }
+        }
+        return searchResult;
+    }
+
+    private class SurahLoader extends AsyncTask<Void, Void, Void> {
+
+        private Activity activity;
+
+        private SurahLoader(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                loadSurahs(activity);
+            } catch (Exception ex) {
+
+            } finally {
+                activity = null;
+            }
+            return null;
+        }
     }
 
 }
