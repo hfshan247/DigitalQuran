26dc8d96d7d242e1deb9b612a6fde8ec4af096e4
M Hussain Farooq <14bscsmfarooq@seecs.edu.pk>
2018-05-02 11:10:07 +0500

    Updated Search Algorith wih each word with Search Class

--- app/src/main/java/com/example/hussainfarooq/digitalquran/SplashScreen.java
+++ app/src/main/java/com/example/hussainfarooq/digitalquran/SplashScreen.java
@@ -1,13 +1,15 @@
 package com.example.hussainfarooq.digitalquran;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.WindowManager;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     private static final int SPLASH_TIME_OUT = 4000;
 
@@ -17,10 +19,15 @@ public class SplashScreen extends AppCompatActivity {
 
         // If the Android version is lower than Jellybean, use this call to hide
         // the status bar.
-        if (Build.VERSION.SDK_INT < 16) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
+//        if (Build.VERSION.SDK_INT < 16) {
+//            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        }
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_splash_screen);
 
         //Splash Screen
